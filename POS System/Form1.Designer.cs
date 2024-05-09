954bfb00f3bab5e68196a0f6124de102e2bd9e14
lkaru <qpalyfe@gmail.com>
2024-05-09 14:06:32 +0900

    2024-05-09
    initation

---
+++ POS System/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace POS_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBL_Title = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BTN_SalesHistory = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LBL_Title
+            // 
+            this.LBL_Title.AutoSize = true;
+            this.LBL_Title.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LBL_Title.Location = new System.Drawing.Point(12, 21);
+            this.LBL_Title.Name = "LBL_Title";
+            this.LBL_Title.Size = new System.Drawing.Size(197, 54);
+            this.LBL_Title.TabIndex = 0;
+            this.LBL_Title.Text = "상품 판매";
+            this.LBL_Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(600, 300);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // BTN_SalesHistory
+            // 
+            this.BTN_SalesHistory.Location = new System.Drawing.Point(21, 415);
+            this.BTN_SalesHistory.Name = "BTN_SalesHistory";
+            this.BTN_SalesHistory.Size = new System.Drawing.Size(150, 25);
+            this.BTN_SalesHistory.TabIndex = 2;
+            this.BTN_SalesHistory.Text = "판매 내역";
+            this.BTN_SalesHistory.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_SalesHistory);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.LBL_Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBL_Title;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BTN_SalesHistory;
+    }
+}
+
